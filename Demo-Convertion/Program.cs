96e11f79c525f10458dd11c89bf3ea104548f738
Legrain Samuel <samuel.legrain@bstorm.be>
2022-03-31 11:38:07 +0200

    Ajoutez des fichiers projet.

---
+++ Demo-Convertion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Demo_Convertion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Veuillez indiquer le nombre d'élèves présents aujourd'hui :");
+            //string nbEleves_input = Console.ReadLine();
+            //int nbEleves;
+            //bool nbEleves_isCorrect = int.TryParse(nbEleves_input, out nbEleves);
+            //Console.WriteLine($"Avons-nous eu une information correct : {nbEleves_isCorrect}");
+            //Console.WriteLine($"Nous avons comptabilisé : {nbEleves} élèves.");
+
+
+            Console.WriteLine("Veuillez indiquer la moyenne des élèves :");
+            string moyenne_input = Console.ReadLine();
+            double moyenne;
+            bool moyenne_isCorrect = double.TryParse(moyenne_input, out moyenne);
+            Console.WriteLine($"Avons-nous eu une information correct : {moyenne_isCorrect}");
+            Console.WriteLine($"Nous avons comptabilisé une moyenne de : {moyenne}/20.");
+            Console.WriteLine($"Nous avons comptabilisé une moyenne de : {(int)moyenne}/20.");
+        }
+    }
+}
---
+++ Demo-Text/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Demo_Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Hello world!";
+            string s2 = s1;
+            Console.WriteLine($"s1 : {s1}");
+            Console.WriteLine($"s2 : {s2}");
+            Console.WriteLine(ReferenceEquals(s1,s2));
+            s1 = "Toto";
+            Console.WriteLine($"s1 : {s1}");
+            Console.WriteLine($"s2 : {s2}");
+            Console.WriteLine(ReferenceEquals(s1, s2));
+
+        }
+    }
+}
