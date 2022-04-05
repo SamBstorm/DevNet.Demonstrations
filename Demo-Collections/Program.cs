bc50f2013b25c2a8d7e07b68f08f995dc87bb29b
Legrain Samuel <samuel.legrain@bstorm.be>
2022-04-05 13:38:57 +0200

    Add Démonstrations Tableau + Collections

---
+++ Demo-Collections/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+
+namespace Demo_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ArrayList listEleves = new ArrayList();     // [ ]
+            ArrayList listEleves = new ArrayList(new string[] { "Benjamin", "Nathalie", "Grégory", "Maïlys", "Bryan", "Ryan", "Sylvain", "Olivier", "Antonio", "Denis", "Nabil", "Paulin" });     // [ ]
+            listEleves.Add("Benjamin");
+            listEleves.Remove("Benjamin");
+
+            //foreach (string eleve in listEleves)
+            //{
+            //    Console.WriteLine(eleve.ToUpper());
+            //}
+
+            for (int i = 0; i < listEleves.Count; i++)
+            {
+                string eleve = (string)listEleves[i];
+                Console.WriteLine(eleve.ToUpper());
+            }
+
+            Hashtable listPresence = new Hashtable();
+
+            foreach (string eleve in listEleves)
+            {
+                listPresence.Add(eleve, true);
+            }
+
+            foreach (string cle in listPresence.Keys)
+            {
+                Console.WriteLine($"{cle} : {listPresence[cle]}");
+            }
+
+            //foreach (bool value in listPresence.Values) {
+            //    Console.WriteLine(value);
+            //}
+
+            foreach (DictionaryEntry entry in listPresence)
+            {
+                Console.WriteLine($"{entry.Key} : {entry.Value}");
+            }
+
+        }
+    }
+}
---
+++ Demo-Tableau/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Demo_Tableau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] eleves = new string[12];
+            //eleves[0] = "Benjamin";
+            //eleves[1] = "Nathalie";
+            //eleves[2] = "Grégory";
+            //eleves[3] = "Maïlys";
+            //eleves[4] = "Bryan";
+            //eleves[5] = "Ryan";
+            //eleves[6] = "Sylvain";
+            //eleves[7] = "Olivier";
+
+            string[] eleves = { "Benjamin","Nathalie","Grégory","Maïlys","Bryan","Ryan","Sylvain","Olivier", "Antonio", "Denis", "Nabil", "Paulin" };
+            bool[] presences = new bool[eleves.Length];
+            for (int i = 0; i < eleves.Length; i++)
+            {
+                Console.WriteLine($"{eleves[i]} : {((presences[i])?"Présent":"Absent")}");
+            }
+
+            //object[,] presencesObj = new object[2, 12];
+            object[,] presenceObj = { 
+                { "Benjamin", "Nathalie", "Grégory", "Maïlys", "Bryan", "Ryan", "Sylvain", "Olivier", "Antonio", "Denis", "Nabil", "Paulin" }, 
+                { false, false, false, false, false, false, false, false, false, false, false, false }
+            };
+
+            for (int i = 0; i < 12; i++)
+            {
+                Console.WriteLine($"{presenceObj[0,i]} : {(((bool)presenceObj[1,i])?"Présent":"Absent")}");
+            }
+            
+
+            
+        }
+    }
+}
