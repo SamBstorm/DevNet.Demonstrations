5d7e22b78a872a3eaf14273d100ed014bae2b9c0
Legrain Samuel <samuel.legrain@bstorm.be>
2022-04-13 16:58:45 +0200

    Add Demonstration Enumerations 02

---
+++ Demo-Enumerations-02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Demo_Enumerations_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez choisir une couleur parmis celles proposées :");
+            string[] colorNames = Enum.GetNames(typeof(ConsoleColor));
+            for (int i = 0; i < colorNames.Length; i++)
+            {
+                Console.WriteLine($"{i} ) {colorNames[i]}");
+            }
+            int user_choice;
+            while (!int.TryParse(Console.ReadLine(), out user_choice)) Console.WriteLine("Veuillez re-sélectionner :");
+            Console.ForegroundColor = (ConsoleColor)user_choice;
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Faites une combinaison de touches :");
+            ConsoleKeyInfo info = Console.ReadKey();
+            while (info.Key != ConsoleKey.F4)
+            {
+                Console.WriteLine($"{info.Key} - {info.Modifiers}");
+                switch (info.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        Console.WriteLine("On monte!");
+                        break;
+                    case ConsoleKey.DownArrow:
+                        Console.WriteLine("On Descend!");
+                        break;
+                    default:
+                        break;
+                }
+                info = Console.ReadKey();
+            }
+
+        }
+    }
+}
