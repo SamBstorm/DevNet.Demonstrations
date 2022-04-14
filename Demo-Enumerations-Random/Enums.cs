368b3d6d88b7660e25897df3bd1e8e687b754a9e
Legrain Samuel <samuel.legrain@bstorm.be>
2022-04-14 10:32:55 +0200

    Add Démonstrations Enumérations Random

---
+++ Demo-Enumerations-Random/Enums.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Enumerations_Random
+{
+    public enum Animals { Cat, Dog, Duck=8, Chicken, Pig, Frog=21, Horse }
+}
---
+++ Demo-Enumerations-Random/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Demo_Enumerations_Random
+{
+    class Program
+    {
+        public static Random RNG = new Random();
+        static void Main(string[] args)
+        {
+            string[] animalsNames = Enum.GetNames(typeof(Animals));
+            int randomId = RNG.Next(0, animalsNames.Length);
+
+            Animals myPet = Enum.Parse<Animals>(animalsNames[randomId]);
+            Console.WriteLine(myPet.ToString());
+        }
+    }
+}
